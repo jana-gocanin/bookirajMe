8ad82f764bdb52eff808aac38c58ab0baf0a407a
jana-gocanin <janagocanin@gmail.com>
2021-11-24 22:05:04 +0100

    dodala jquery na rent a car

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -40,26 +40,6 @@ x.innerHTML = "Latitude: " + position.coords.latitude +
 
 // jquery
 
-$( function() {
-    var state = true;
-    $( "#button" ).on( "click", function() {
-      if ( state ) {
-        $( "#effect" ).animate({
-          backgroundColor: "#aa0000",
-          color: "#fff",
-          width: 500
-        }, 1000 );
-      } else {
-        $( "#effect" ).animate({
-          backgroundColor: "#fff",
-          color: "#000",
-          width: 240
-        }, 1000 );
-      }
-      state = !state;
-    });
-  } );
-
-  $(function() {
+  $(document).ready(function() {
     $(".rslides").responsiveSlides();
   });

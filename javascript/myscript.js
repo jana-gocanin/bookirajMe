955831358d37d20fc635f0531901ed6b554b9c18
jana-gocanin <janagocanin@gmail.com>
2021-11-24 21:47:00 +0100

    dodajem jquery plugin

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -37,3 +37,29 @@ function showPosition(position) {
 x.innerHTML = "Latitude: " + position.coords.latitude +
 "<br>Longitude: " + position.coords.longitude;
 }
+
+// jquery
+
+$( function() {
+    var state = true;
+    $( "#button" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+  } );
+
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
